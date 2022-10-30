42c84e54a1ea158eb863a6b2e5261dacd703a565
id2k1149 <id2k1149@gmail.com>
2022-10-30 11:28:31 -0400

    add label and button to welcome screen

--- AppWithLogin/LoginViewController.swift
+++ AppWithLogin/LoginViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let userName = "User"
+    private let password = "password"
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+//        welcomeVC.minValue = minValueLabel.text
+           }
 
-
+    @IBAction func logInButtonTapped() {
+//        if userNameTF == userName && passwordTF == password {
+//
+//        }
+    }
+    
 }
 
