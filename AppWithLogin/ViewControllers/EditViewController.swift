b86c393b5a223982db5896a8faa1b85cc05deffe
id2k1149 <id2k1149@gmail.com>
2022-11-01 14:27:24 -0400

    add EditViewController

---
+++ AppWithLogin/ViewControllers/EditViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EditViewController.swift
+//  AppWithLogin
+//
+//  Created by Max Franz Immelmann on 11/1/22.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    @IBOutlet var nameTF: UITextField!
+    @IBOutlet var surnameTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
